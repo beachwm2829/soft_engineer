30bd42e9c875035364a1ae17189261a5d9f0e3e8
DE <DE@DESKTOP-7DK6S6K>
2020-03-27 22:57:12 +0700

    detail

--- FormRegister.php
+++ FormRegister.php
@@ -2,9 +2,9 @@
 <head>
     <link rel="stylesheet" type="text/css" href="css/table.css">
     <title> Boy & Girl </title>
-    <script src="js/checkinput.js" ></script>
 </head>
 <body>
+    <script src="js/checkinput.js" ></script>
     <?php
         error_reporting(E_ALL ^ E_NOTICE); 
         $s = $_REQUEST['s'];
@@ -14,39 +14,38 @@
         ?>
     <div class="box">
         <div id="container">
-            <form name="Formregister" action="checkaction.php" method="POST" onsubmit="return check()">
+            <form action="checkaction.php" method="POST" name="formjs">
             
             <label class="LabelText" for="id">Username</label>
           
             <?php
-                        echo "<input type='text' id='user'  name ='user' maxlength=20 value=''>";
+                        echo "<input type='text' id='user'  name ='user'>";
              ?>
             <label class="LabelText" for="id">Password</label>
           
             <?php
-                        echo "<input type='password' id='pass'  name ='pass' maxlength=20 value=''>";
+                        echo "<input type='password' id='pass'  name ='pass'>";
              ?>
             <label class="LabelText" for="id">ชื่อ</label>
             <?php
-                        echo "<input type='text' id=fname name ='name' value=''>";
+                        echo "<input type='text' id='fname' name ='name'>";
              ?>
             <label class="LabelText" for="id">เบอร์</label>
             <?php
-                        echo "<input type='text' id=phone name ='phone' value=''>";
+                        echo "<input type='text' id='phone' name ='phone'>";
              ?>
            
             <label class="LabelText" for="id">ที่อยู่</label>
             <?php
-                        echo "<input type='text' id=address name ='address' maxlength=10 value=''>";
+                        echo "<input type='text' id='address' name ='address' maxlength=10>";
              ?>
             <label class="LabelText" for="id">เพศ</label>
             <?php
                         echo "<input type=radio  name=gender value='male'> Male";
                         echo "<input type=radio  name=gender value='female'> FeMale <br>";
              ?>
-                 <?php 
-                    echo "<br><input type='submit' name='submit' value='".$text."'>";
-                    ?>
+            <br><input type='submit' name='submit' value="<?php echo $text; ?>" onclick="return check()">
+                    
     </form>
         </div>
     </div>
