26f8e0d8dee4a721a7a988ac9066e93a0ef06f92
Lynjee <Lynjee@DESKTOP-86O1QEA>
2020-03-28 20:46:09 +0700

    หน้า Admin

--- FormAdmin.php
+++ FormAdmin.php
@@ -18,17 +18,19 @@
             ?>
         <form action="checkaction.php"  method="POST">
         <table>
-            <tr><th>แก้ไข</th><th>Username</th><th>ชื่อ</th><th>เพศ</th><th>เบอร์โทร</th><th>ที่อยู่</th></tr>
+            <tr><th>ID</th><th>Username</th><th>ชื่อ</th><th>เพศ</th><th>เบอร์โทร</th><th>ที่อยู่</th><th>แก้ไข</th><th>ลบ</th></tr>
             <?php
             while($row = mysqli_fetch_array($objquery)){
                 echo "<tr>";
                 //echo "<td><input type='checkbox' name='checkbox[]' value='".$row['mid']."'</td>";
-                echo "<td><a href=FormInput.php?s=1&user=".$row['user_id'].">แก้ไข</a></td>";
+                echo "<td>".$row['user_id']."</td>";
                 echo "<td>".$row['user_username']."</td>";
                 echo "<td>".$row['user_name']."</td>";
                 echo "<td>".$row['user_sex']."</td>";
                 echo "<td>".$row['user_tel']."</td>";
                 echo "<td>".$row['user_address']."</td>";
+                echo "<td><a href=FormInput.php?s=1&user=".$row['user_id'].">แก้ไข</a></td>";
+                echo "<td><input type='checkbox' name='checkbox[]' value='".$row['user_id']."'</td>";
                 echo "</tr>";
             }
             ?>
@@ -39,6 +41,7 @@
             ?>
         </table>
         <center>
+            <input 
             <button class ="button button1"><a href="FormInput.php"</a>เพิ่มข้อมูล</button>
             <button class ="button button3"><a href="login.php" </a>Logout</button>
         </center>
